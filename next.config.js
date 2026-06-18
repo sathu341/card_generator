6f120e8280ee51356b487a2f58cec503ee941bb7
sathu341 <sathishchandran321@gmail.com>
2026-06-18 16:51:11 +0530

    sec commit

--- next.config.js
+++ next.config.js
@@ -3,9 +3,9 @@ const nextConfig = {
   images: {
     domains: ['drive.google.com', 'lh3.googleusercontent.com'],
   },
-  api: {
-    bodyParser: false,
-  },
+  // api: {
+  //   bodyParser: false,
+  // },
 };
 
 module.exports = nextConfig;
